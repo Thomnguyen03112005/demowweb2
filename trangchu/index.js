e1b008eb39dcd344aa6345a41ee50a45c6aa86ad
Thowm <83901352+Thomnguyen03112005@users.noreply.github.com>
2022-02-17 17:09:24 +0700

    Add files via upload

---
+++ trangchu/index.js
@@ -0,0 +1,8 @@
+/*var id = setTimeout(mytime, 5000);
+function id () {
+    document.getElementById("load").hidden()
+}*/
+/*var time = setTimeout(timeee, 5000);
+function timeee() {
+    document.getElementById("efect").style.overflow = hidden;
+}*/
